a820d7bb5b8142d8ac95cc88f6d52371b01af81c
VitorMScolari <vmarcals@hotmail.com>
2021-08-06 18:26:41 -0300

    script fix

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const buttons = document.querySelectorAll('[data-time]');
 function timer(seconds) {
     clearInterval(countdown);
 
-
-
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds)
@@ -21,30 +19,38 @@ function timer(seconds) {
             return;
         }
 
-        displayTimeLeft(seconds)
+        displayTimeLeft(secondsLeft)
     }, 1000);
 }
 
 function displayTimeLeft(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
-    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
     document.title = display;
     timerDisplay.textContent = display;
-}
+  }
 
 function displayEndTime(timestamp) {
     const end = new Date(timestamp);
     const hour = end.getHours();
     const minutes = end.getMinutes();
-    endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
 
 
 function startTimer() {
-    const seconds = parseInt(this.dataset.time)
+    const seconds = parseInt(this.dataset.time);
     timer(seconds);
 }
 
 
-buttons.forEach(button => button.addEventListener('click', startTimer))
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins*60);
+    this.reset();
+});
